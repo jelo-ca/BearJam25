63a085ac2b7b1319914d2dadaf0c60aa2b30ce5e
TheGrandBraker <harrisontruong23@gmail.com>
2025-03-08 02:50:12 -0800

    Smooth movement and fixed the ground thingy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     [Header("Movemment Settings")]
     [SerializeField] float jumpForce = 5f;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveResponsiveness = 5f;
+
     [SerializeField] float fallSpeed = 5f;
 
     [Header("Keybinds")]
@@ -44,7 +46,9 @@ public class PlayerController : MonoBehaviour
 
     private void ProcessInput()
     {
-        transform.position = new Vector3(transform.position.x + xInput * Time.deltaTime * moveSpeed, transform.position.y);
+        // hehe, funky math :3
+        rb.linearVelocity = rb.linearVelocity + Vector2.right * (xInput * moveSpeed * Vector2.right - rb.linearVelocity) * (1-Mathf.Exp(-moveResponsiveness*Time.deltaTime));
+        //transform.position = new Vector3(transform.position.x + xInput * Time.deltaTime * moveSpeed, transform.position.y);
     }
 
     private void Jump()
@@ -55,10 +59,15 @@ public class PlayerController : MonoBehaviour
     private void CheckGround()
     {
         // Cast a ray straight down.
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, .5f);
-        Debug.DrawRay(transform.position, -Vector2.up * .5f, Color.green);
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 2.3f);
+
+        //Vector2 characterSize = new Vector2(2.991782f, 4.42372f); // there must be a way to get this right?
+
+        //RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.right * characterSize, 0, Vector2.down, .5f);
 
         // If it hits something...
-        isGrounded = (hit.collider.CompareTag("Floor")) ? true : false;
+        //Debug.Log(hit.collider.tag);
+        isGrounded = (Physics2D.Raycast(transform.position, -Vector2.up, 1.3f, LayerMask.GetMask("Floor Layer"))) ? true : false;
+        Debug.DrawRay(transform.position, -Vector2.up * 1.3f, Color.green);
     }
 }
