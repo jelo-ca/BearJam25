2f90de4c82c1c39ee0bba8ce76488a85d8c6652e
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2025-03-07 23:01:40 -0800

    updated controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,17 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private float xInput;
 
+    [Header("Movemment Settings")]
     [SerializeField] float jumpForce = 5f;
     [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float fallScale = 5f;
+    [SerializeField] float fallSpeed = 5f;
 
+    [Header("Keybinds")]
     [SerializeField] KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] KeyCode resetKey = KeyCode.R;
+
+    public bool isGrounded = true;
+
     
     void Start()
     {
@@ -21,6 +27,9 @@ public class PlayerController : MonoBehaviour
     {
         GetInput();
         ProcessInput();
+        CheckGround();
+
+        Debug.Log(isGrounded);
     }
 
     private void GetInput()
@@ -38,11 +47,21 @@ public class PlayerController : MonoBehaviour
         transform.position = new Vector3(transform.position.x + xInput * Time.deltaTime * moveSpeed, transform.position.y);
 
         Debug.Log(rb.linearVelocityY);
-        rb.gravityScale = (rb.linearVelocityY < 1) ? fallScale : 1;
+        rb.linearVelocityY = ((rb.linearVelocityY < 1) && !isGrounded) ? -fallSpeed : rb.linearVelocityY;
     }
 
     private void Jump()
     {
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        // Cast a ray straight down.
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, .5f);
+        Debug.DrawRay(transform.position, -Vector2.up * .5f, Color.green);
+
+        // If it hits something...
+        isGrounded = (hit.collider != null) ? true : false;
     }
 }
